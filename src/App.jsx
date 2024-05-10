e7415716c15ba9267b36645fba5a46dd96708620
Burak <burakkahyabk@gmail.com>
2024-05-10 21:15:16 +0300

    Using same state in different parts. added player turn change logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import GameBoard from './components/GameBoard';
 import Player from './components/Player';
+import {useState} from 'react';
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((currentlyActivePlayer) =>
+      currentlyActivePlayer === 'X' ? 'O' : 'X'
+    );
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="player1" symbol="X" />
-          <Player initialName="player2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="player1"
+            symbol="X"
+            isActive={activePlayer === 'X'}
+          />
+          <Player
+            initialName="player2"
+            symbol="O"
+            isActive={activePlayer === 'O'}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
     </main>
   );
