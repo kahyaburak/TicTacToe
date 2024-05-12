c12e87e71772d978dce161ee5488dda0d33867f0
Burak <burakkahyabk@gmail.com>
2024-05-12 15:08:42 +0300

    disabled gameboard buttons conditionally

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -37,7 +37,9 @@ export default function GameBoard({onSelectSquare, turns}) {
           <ol>
             {row.map((playerSymbol, columnIndex) => (
               <li key={columnIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, columnIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, columnIndex)}
+                  disabled={playerSymbol ? true : false}>
                   {playerSymbol}
                 </button>
               </li>
